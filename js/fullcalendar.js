8afa75d6bc14a7f183400f674110574f2a09d773
mike <mikekarpenko@gmail.com>
2022-11-03 14:48:54 +0200

    test data from zoho

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -1,14 +1,70 @@
 document.addEventListener('DOMContentLoaded', async function () {
     console.log('start input');
 
-    // ZOHO.embeddedApp.init();
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        // initialDate: '2022-08-07',
+        selectable: true,
+        editable: true,
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2022-08-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2022-08-07',
+                end: '2022-08-10'
+            },
+            {
+                groupId: '999',
+                title: 'Repeating Event',
+                start: '2022-08-09T16:00:00'
+            },
+            {
+                groupId: '999',
+                title: 'Repeating Event',
+                start: '2022-08-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2022-08-11',
+                end: '2022-08-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2022-08-12T10:30:00',
+                end: '2022-08-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2022-08-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2022-08-12T14:30:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2022-08-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2022-08-28'
+            }
+        ]
+    });
+    calendar.render();
 
 
-    ZOHO.embeddedApp.on("Dial",function(data){
-        console.log("Number Dialed");
-    })
     ZOHO.embeddedApp.on("PageLoad",function(data){
-        console.log(data);
+        console.log('data= '+data);
     })
     ZOHO.embeddedApp.init();
 })
