9626892ac458090962a1b5612908f121524bafb1
mike <mikekarpenko@gmail.com>
2022-11-03 14:54:25 +0200

    test data from zoho

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async function () {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('start input');
 
     calendarEl = document.getElementById('calendar');
