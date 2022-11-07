9bfa675ec2d7d4a861f4bab9730fecd6fccb2fcc
mike <mikekarpenko@gmail.com>
2022-11-07 15:03:51 +0200

    test item data view

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 // calendar.render();
 
                 // ZOHO.CRM.API.searchRecord({Entity:"Sales_Orders", Type:"Vendor.id",Query: this.getAttribute('data-id')})
-                ZOHO.CRM.API.searchRecord({Entity:"Sales_Orders", Type:"Vendors3", Query: "Acosta Painting LLC (Denis)"})
+                ZOHO.CRM.API.searchRecord({Entity:"Sales_Orders", criteria: "Vendors3", Query: "Acosta Painting LLC (Denis)"})
                     .then(function(data){
                         console.log('data= ' + data)
                     })
