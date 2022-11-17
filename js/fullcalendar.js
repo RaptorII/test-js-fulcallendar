95c669632a65aa6bfcf1b29d4ac305856408cc1b
mike <mikekarpenko@gmail.com>
2022-11-17 17:37:52 +0200

    extend borders for edit timelines

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let calendarEl = document.getElementById('calendar');
     let calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
-        // initialDate: '2022-08-07',
+        initialDate: '2022-08-07',
         selectable: true,
         editable: true,
         headerToolbar: {
@@ -13,7 +13,32 @@ document.addEventListener('DOMContentLoaded', function () {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
-        events: []
+        events: [
+            /*{
+               title: 'All Day Event',
+               start: '2022-08-01'
+            },
+            {
+               title: 'Long Event',
+               start: '2022-08-07',
+               end: '2022-08-10'
+            },
+            {
+               groupId: '999',
+               title: 'Repeating Event',
+               start: '2022-08-09T16:00:00'
+            },
+            {
+               groupId: '999',
+               title: 'Repeating Event',
+               start: '2022-08-16T16:00:00'
+            },
+            {
+               title: 'Conference',
+               start: '2022-08-11',
+               end: '2022-08-13'
+            }*/
+        ]
 
     });
     calendar.render();
@@ -22,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // console.log('data= '+data);
 
         var vendor_list = [];
-        var jobsheet_list = [];
+        // var jobsheet_list = [];
         await ZOHO.CRM.API.getAllRecords({ Entity: "Vendors", sort_order: "asc", per_page: 100, page: 1 })
             .then(async function (vendor_detail) {
                 // console.log("Employee Record");
