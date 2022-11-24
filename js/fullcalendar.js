3c5e9439d564f642ee45eef9a98cc9c03ce7f7e8
mike <mikekarpenko@gmail.com>
2022-11-24 15:32:36 +0200

    work on okBtn

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -304,7 +304,9 @@ document.addEventListener('DOMContentLoaded', function () {
                         }
                         // console.log('vendor_list: ' +  JSON.stringify(vendor_list));
                         // console.log('get_all_accounts.length: ' + get_all_accounts.length);
-                        main_account_list = main_account_list.concat(account_list);
+                        if( account_list.length !== undefined ) {
+                            main_account_list = main_account_list.concat(account_list);
+                        }
                     }
                 })
         }
