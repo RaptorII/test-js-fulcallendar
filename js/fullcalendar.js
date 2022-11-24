a92f4c20f538d792760c1cab2cabeb8d0deabf31
mike <mikekarpenko@gmail.com>
2022-11-24 14:26:28 +0200

    work on okBtn

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -293,10 +293,10 @@ document.addEventListener('DOMContentLoaded', function () {
         for(let page = 0; page <= 10; page++) {
             await ZOHO.CRM.API.getAllRecords({Entity: "Accounts", sort_order: "asc", per_page: 200, page: page })
                 .then(async function (account_detail) {
-                    if (account_detail.data.length) {
-                        var get_all_accounts = account_detail.data;
+                    if (!isNaN(account_detail.data.length)) {
+                        let get_all_accounts = account_detail.data;
                         for (let i = 0, l = get_all_accounts.length; i < l; i++) {
-                            var account_object = {};
+                            let account_object = {};
                             account_object.id = get_all_accounts[i].id;
                             account_object.title = get_all_accounts[i].Account_Name;
                             account_list.push(account_object);
